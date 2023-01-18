b3830dd3796ea7b32f9554b5d955f0546b7c3943
ShadyAV <jshadymail@gmail.com>
2023-01-18 14:03:08 +0700

    Add hover effect for grid cells

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
 const gridDiv = document.getElementById("grid");
 const sizeButton = document.getElementById("sizeButton");
+const gridDivs = document.querySelectorAll('.gridDiv');
 
 sizeButton.addEventListener("click", function openPromtWindow() {
-    let size = prompt("Please, enter the size of the grid", "64")
-    const gridDivs = document.querySelectorAll('.gridDiv');
+    let size = prompt("Please, enter the size of the grid", "64");
+    deleteDivs();
+    createDivs(size);
+});
+
+function deleteDivs() {
     gridDivs.forEach(div => {
         div.remove();
     });
-    createDivs(size);
-});
-createDivs();
+}
+
+function paintDiv(newDiv) {
+    newDiv.addEventListener("mouseenter", function changeColor() {
+        newDiv.style.backgroundColor = "black";
+    });
+}
+
 function createDivs(size = 16) {
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            const newDiv = document.createElement("div");
-            gridDiv.appendChild(newDiv);
-            newDiv.style.border = "1px solid #0000FF";
-            newDiv.classList.add("gridDiv");
-        }
+    for (let i = 0; i < size * size; i++) {
+        const newDiv = document.createElement("div");
+        gridDiv.appendChild(newDiv);
+        newDiv.classList.add("gridDiv");
+        paintDiv(newDiv);
     }
     let root = document.documentElement;
     root.style.setProperty("--divs", size);
+}
+
+window.onload = () => {
+    createDivs();
 }
 
 
