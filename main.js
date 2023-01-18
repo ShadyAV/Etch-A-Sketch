18d994f957a841c7a64f2253e5c5896d44a650a4
ShadyAV <jshadymail@gmail.com>
2023-01-19 02:54:08 +0700

    Add erase and clear button

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const DEFAULT_COLOR = "#333333";
 const DEFAULT_MODE = "color";
+const DEFAULT_SIZE = "16";
 
 let currentColor = DEFAULT_COLOR;
 let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
 
 function setCurrentMode(newMode) {
     currentMode = newMode;
@@ -12,22 +14,27 @@ function setCurrentColor(newColor) {
     currentColor = newColor;
 }
 
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+}
+
 const gridDiv = document.getElementById("grid");
 const sizeButton = document.getElementById("sizeButton");
 const gridDivs = document.querySelectorAll('.gridDiv');
 const colorPicker = document.getElementById("colorPicker");
 const rgbButton = document.getElementById("rgbButton");
+const eraseButton = document.getElementById("eraseButton");
+const clearButton = document.getElementById("clearButton");
 
 sizeButton.addEventListener("click", () => {
-    let size = prompt("Please, enter the size of the grid", "64");
+    let currentSize = prompt("Please, enter the size of the grid", "64");
     deleteDivs();
-    createDivs(size);
+    setCurrentSize(currentSize);
+    createDivs(currentSize);
 });
 
 function deleteDivs() {
-    gridDivs.forEach(div => {
-        div.remove();
-    });
+    gridDiv.innerHTML = "";
 }
 
 let isDown = false
@@ -37,21 +44,33 @@ document.body.onmouseup = () => (isDown = false);
 colorPicker.oninput = (e) => setCurrentColor(e.target.value);
 colorPicker.onclick = () => setCurrentMode("color");
 rgbButton.onclick = () => setCurrentMode("rainbow");
+eraseButton.onclick = () => setCurrentMode("erase");
+clearButton.onclick = () => clearDivs();
 
 function paintDiv(e) {
-    if (e.type = "mouseover" && !isDown) return;
-    if (currentMode === 'rainbow') {
-        const randomR = Math.floor(Math.random() * 256)
-        const randomG = Math.floor(Math.random() * 256)
-        const randomB = Math.floor(Math.random() * 256)
+    if (e.type === "mouseover" && !isDown) return;
+    if (currentMode === "rainbow") {
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
         e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
     }
-    else if (currentMode === 'color') {
+    else if (currentMode === "color") {
         e.target.style.backgroundColor = currentColor;
+    }
+    else if (currentMode === "erase") {
+        e.target.style.backgroundColor = "#e6e6e6";
     }
 }
 
+function clearDivs() {
+    gridDiv.innerHTML = "";
+    createDivs(currentSize);
+}
+
 function createDivs(size = 16) {
+    gridDiv.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    gridDiv.style.gridTemplateRows = `repeat(${size}, 1fr)`
     for (let i = 0; i < size * size; i++) {
         const newDiv = document.createElement("div");
         gridDiv.appendChild(newDiv);
